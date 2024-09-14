42c66b70693d8232b560f424299b1e36e19d0cb2
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-14 16:06:54 -0400

    event object

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,19 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('Hello, world');
+    const handleClick = (e) => {
+        console.log('Hello, world', e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => {
-                handleClickAgain('mario')
+            <button onClick={(e) => {
+                handleClickAgain('mario', e)
             }}>Click me again</button>
         </div>
      );
