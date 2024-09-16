04d78f49e9e96b0969db856b69df5d2cbe172c41
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-16 10:27:00 -0400

    Conditional Loading Message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
+  const [isPending, setIsPending] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -10,17 +11,21 @@ const Home = () => {
 };
 
   useEffect(() => {
-    fetch('http://localhost:8002/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8002/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
         setBlogs(data);
+        setIsPending(false);
       })
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
     </div>
   );
