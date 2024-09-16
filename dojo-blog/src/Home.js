d2979a82ec6586001d4a4b2dfae8a41fa393b4f1
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-16 18:43:43 -0400

    error checking

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -12,21 +13,29 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8002/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      })
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsPending(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsPending(false);
+        })
     }, 1000);
   }, [])
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
-      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+      { error && <div>{error}</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} /> }
     </div>
   );
 }
