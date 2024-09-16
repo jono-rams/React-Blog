bd1117696729dc9503af37d9d0e2d2a44e0d344b
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-16 19:36:31 -0400

    useEffect cleanup

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -6,27 +6,35 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        setTimeout(() => {
-          fetch(url)
-            .then(res => {
-              if(!res.ok) {
-                throw Error('Could not fetch the data for that resource');
-              }
-              return res.json();
-            })
-            .then(data => {
-              setData(data);
-              setIsPending(false);
-              setError(null);
-            })
-            .catch(err => {
-              setIsPending(false);
-              setError(err.message);
-            })
-        }, 1000);
-      }, [url])
+        const abortCon = new AbortController();
 
-      return { data, isPending, error }
+        setTimeout(() => {
+            fetch(url, { signal: abortCon.signal })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
+                })
+        }, 1000);
+
+        return () => abortCon.abort();
+    }, [url])
+
+    return { data, isPending, error }
 }
 
 export default useFetch;
